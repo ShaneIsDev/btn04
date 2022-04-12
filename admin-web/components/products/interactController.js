34f1564ddb3701702b57c3670325f6f6947d6fc7
19127558 <98400325+ShaneIsDev@users.noreply.github.com>
2022-04-12 21:49:49 +0700

    final commit

--- admin-web/components/products/interactController.js
+++
@@ -1,7 +0,0 @@
-const interactService=require('./interactService')
-exports.listShow = (req, res, next) => {
-    res.render("/list");
-  };
-  exports.postShow = (req, res, next) => {
-    res.render("/post");
-  };
--- admin-web/components/products/interactService.js
+++
@@ -1,9 +0,0 @@
-const { products } = require("../../models");
-
-exports.postnew = function (prodname, price,img_url) {
-    return products.create({
-      name: prodname,
-      price: price,
-      img: img_url,
-    });
-  };
